0881b514fa888833450ed9ba3ac07319292d3999
jahid-111 <jahidjob5@outlook.com>
2024-12-31 05:40:21 +0300

    Modify: SearchBar-UI

--- src/components/form/SearchForm.jsx
+++ src/components/form/SearchForm.jsx
@@ -2,7 +2,7 @@ const SearchForm = () => {
   return (
     <div className="w-full bg-opacity-80 backdrop-blur-sm ">
       <input
-        className="w-full text-sm bg-gray-700 h-[2.75rem] px-4 rounded-full outline-none text-gray-200 bg-opacity-80 placeholder-gray-100"
+        className="w-full text-[1.1rem] font-semibold focus:ring-primary focus:ring-1 duration-500 bg-gray-700 h-[2.75rem] px-4 rounded-full outline-none text-gray-200 bg-opacity-80 placeholder-gray-100"
         type="text"
         placeholder="Search..."
       />
